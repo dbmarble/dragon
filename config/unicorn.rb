3930653a83b2163e35f526735b0d8057823ca326
David Marble <davidbmarble@gmail.com>
2012-05-22 21:23:24 -0700

    deployment configs2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.meetingfix.sock"
+listen "/tmp/unicorn.dragon.sock"
 worker_processes 2
 timeout 30
