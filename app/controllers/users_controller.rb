12ccc3f6013204056af99e68cf08b62e5ef90eb5
David Marble <davidbmarble@gmail.com>
2012-05-31 11:18:33 -0700

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  render :layout => 'application'
+
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
