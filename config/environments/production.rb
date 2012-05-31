64dfe0a409c8f191312b71698320ea895c119472
David Marble <davidbmarble@gmail.com>
2012-05-31 14:21:21 -0700

    email5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,18 +61,18 @@ Dragon::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'meetingfix.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "meetingfix.com",
+    domain: "google.com",
     user_name: ENV["contact@meetingfix.com"],
     password: ENV["W1ldc@ts"],
     authentication: "plain",
