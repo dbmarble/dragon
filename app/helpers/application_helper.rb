ea744ac336c14e5f0b25c38602bb708c76e14449
David Marble <davidbmarble@gmail.com>
2012-06-13 17:10:09 -0700

    fixed descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,12 @@
 module ApplicationHelper
 
-# Returns the full title on a per-page basis.       # Documentation comment
-  def full_title(page_title)                          # Method definition
-    base_title = "MeetingFix"  # Variable assignment
-    if page_title.empty?                              # Boolean test
-      base_title                                      # Implicit return
-    else
-      "#{base_title} | #{page_title}"                 # String interpolation
-    end
+  def title(page_title)
+    content_for(:title) { page_title }
   end
-
 
   def description(page_description)
     content_for(:description) { page_description}
   end
+
 
 end
